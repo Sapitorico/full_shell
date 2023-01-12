49e0816c6c16daa08806bdfa378fd1023e75f988
Sapitorico <renzoyaques@gmail.com>
2023-01-12 17:54:43 -0300

    create tokenize and free for the tokenizer matrix

--- src/main.c
+++ src/main.c
@@ -1,18 +1,24 @@
 #include "main.h"
 int argument_counter(char *input);
+void tokenize(int counter);
+void free_tokens();
 
-char **command = NULL;
+char *input = NULL;
+char **tokenize_input = NULL;
 
 int main(void)
 {
-	char *input = NULL;
 	size_t size_input = 0;
 	int isat = isatty(STDIN_FILENO);
+	int counter = 0;
 	
 	isat && write(1, "$ ", 2);
 	while (getline(&input, &size_input, stdin) >= 0)
 	{
 		isat && write(1, "$ ", 2);
+		if ((counter = argument_counter(strdup(input))) == 0) continue;
+		tokenize(counter);
+		free_tokens();
 	}
 	if (input) free(input);
 	return (0);
@@ -20,12 +26,32 @@ int main(void)
 int argument_counter(char *copy_input)
 {
 	int counter = 0;
-	char *tok = strtok(copy_input, " \t\n");
+	char *tok = strtok(copy_input, DELIM);
 	while (tok)
 	{
 		counter++;
-		tok = strtok(NULL, " \t\n");
+		tok = strtok(NULL, DELIM);
 	}
 	free(copy_input);
 	return counter;
+}
+
+void tokenize(int counter)
+{
+	char *token = strtok(input, DELIM);
+	tokenize_input = malloc(sizeof(char*) * (counter + 1));
+	if (!tokenize_input) exit(98);
+	int i = 0;
+	while(token)
+	{
+		tokenize_input[i] = token;
+		token = strtok(NULL, DELIM);
+		i++;
+	}
+	tokenize_input[i] = NULL;
+}
+void free_tokens()
+{
+	if (!tokenize_input) return;
+	free(tokenize_input);
 }
--- src/main.h
+++ src/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include <unistd.h>
+#define DELIM " \t\n"
 #endif
